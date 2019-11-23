64e24b9d2c323e50ef3ae2bc30c627ee06ae0730
imthaghost <imthaghost@protonmail.com>
2019-11-22 22:10:43 -0800

    [update] user model-database migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class user(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    profile_pic = models.ImageField()
+    recent_ip = models.GenericIPAddressField()
