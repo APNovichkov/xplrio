e43f3350d337fa21c6d526f3ed945467884c5640
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-12-09 11:23:13 -0800

    [fix] Syntax fix

--- xplrio/settings.py
+++ xplrio/settings.py
@@ -141,7 +141,7 @@ LOGOUT_REDIRECT_URL = "/login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
