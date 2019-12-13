cfde4dfb273441b5aa6a9db69da8b673c4b7cb65
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-12-12 20:42:50 -0800

    [add] Added xplrmain models to admin page

--- xplrmain/admin.py
+++ xplrmain/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import UserPost
+from .models import UserPost, UserGoPost, UserVisitedPost, UserSavedPost
 
 # Register your models here.
 admin.site.register(UserPost)
+admin.site.register(UserGoPost)
+admin.site.register(UserVisitedPost)
+admin.site.register(UserSavedPost)
