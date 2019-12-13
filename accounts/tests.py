16ce598806c3c4fc96b0546d28e43ebd50f5415f
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-12-12 20:25:33 -0800

    [add] Added tests to xplrmain and accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
+
 # Create your tests here.
+class AccountsUserProfileTest(TestCase):
+    def test_true_is_true(self):
+        self.assertEqual(True, True)
+
+    def test_user_profile_creation(self):
+        user = User()
+        user.save()
+
+        user_profile = UserProfile(
+            user=user,
+            first_name="Andrey",
+            last_name="Novichkov",
+            email="andreynovichkov@gmail.com",
+            photography_type="Beginner",
+            risk_level="High",
+            profile_pic=None,
+            recent_ip="127.0.0.1")
+
+        user_profile.save()
+
+        get_user_profile = UserProfile.objects.get(user=user)
+
+        self.assertEqual(get_user_profile.first_name, 'Andrey')
+
+class AccountsTestAuthentication(TestCase):
+    def test_login_page(self):
+        response = self.client.get("/login/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("username", str(response.context['form']))
--- xplrmain/tests.py
+++ xplrmain/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from xplrmain.models import UserPost
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class XplrmainNewpostTest(TestCase):
+    def test_true_is_true(self):
+        self.assertEqual(True, True)
+
+    def test_new_post_saves(self):
+        user = User()
+        user.save()
+
+        user_post = UserPost(
+            user=user,
+            username="APNovichkov",
+            coordinates="123, 234",
+            difficulty="Low",
+            description="Cool spot bro",
+            time_to_go="Never",
+            verified=False)
+
+        user_post.save()
+
+        get_user_post = UserPost.objects.get(user=user)
+
+        self.assertEqual(get_user_post.username, "APNovichkov")
